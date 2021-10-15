72a5bad08e4eadafa70822483df16bc711b60ab1
relick <relick@users.noreply.github.com>
2021-10-15 11:27:36 +0100

    Slightly simplify MakeArray.

--- src/common/MakeArray.h
+++ src/common/MakeArray.h
@@ -8,19 +8,18 @@ namespace detail
 // Make it possible to forward types that match but cast those that don't
 template< typename From, typename To > requires std::convertible_to< From, To >
 struct ConvertOrForwardHelper { using Type = To; };
-template< typename From, typename To > requires std::same_as< From, To >
+template< typename From, typename To > requires std::same_as< std::remove_reference_t< From >, std::remove_reference_t< To > >
 struct ConvertOrForwardHelper< From, To > { using Type = To&&; };
 
 template< typename From, typename To >
 using ConvertOrForwardType = typename ConvertOrForwardHelper< From, To >::Type;
 }
 
-template< typename T, typename F, typename ...U >
-constexpr auto MakeArray( F&& arg1, U&&... args )
-	requires ( std::convertible_to< F, T > && ( std::convertible_to< U, T > && ... ) )
+template< typename T, typename ...U >
+constexpr auto MakeArray( U&&... args )
+	requires ( std::convertible_to< U, T > && ... )
 {
-	return std::array< T, 1 + sizeof...( args ) > {
-		static_cast< detail::ConvertOrForwardType< F, T > >( std::forward< F >( arg1 ) ),
+	return std::array< T, sizeof...( args ) > {
 		static_cast< detail::ConvertOrForwardType< U, T > >( std::forward< U >( args ) )...
 	};
 }
